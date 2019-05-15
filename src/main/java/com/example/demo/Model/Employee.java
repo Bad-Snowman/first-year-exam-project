71190b1c0ba7eb9124cd3a961db3f3d1c19b7baa
Mfajs2609 <math@24i5@stud.kea.dk>
2019-05-15 14:51:01 +0200

    Employee class med attributer, constructor og getters and setters

--- src/main/java/com/example/demo/Model/Employee.java
+++ src/main/java/com/example/demo/Model/Employee.java
@@ -16,7 +16,7 @@ public class Employee {
         this.employeeEmail = employeeEmail;
     }
 
-    ///////////////////////////////Getters and Setter///////////////////
+    ///////////////////////////////Getters and Setter/////////////////////
     public int getEmployeeID() {
         return employeeID;
     }
