f74bb2b57aebf0983d168a1a5c1ee7718af75bff
Oliver <oliv603m@stud.kea.dk>
2019-05-16 11:57:04 +0200

    Merge branch 'master' of C:\Users\Oliver\Documents\GitHub\first-year-exam-project with conflicts.

--- src/main/java/com/example/demo/Service/ProjectService.java
+++ src/main/java/com/example/demo/Service/ProjectService.java
@@ -1,11 +1,34 @@
 package com.example.demo.Service;
 
+import com.example.demo.Model.Project;
 import com.example.demo.Repository.ProjectRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProjectService {
     @Autowired
     ProjectRepo projectRepo;
+
+    public List<Project> fetchAll(){
+        return projectRepo.fetchAll();
+    }
+
+    public Project addProject(Project p){
+        return projectRepo.addProject(p);
+    }
+
+    public Project findProjectById(int projectID){
+        return projectRepo.findProjectById(projectID);
+    }
+
+    public Boolean deleteProject(int projectID){
+        return projectRepo.deleteProject(projectID);
+    }
+
+    public Project updateProject(int projectID, Project p){
+        return projectRepo.updateProject(projectID, p);
+    }
 }
