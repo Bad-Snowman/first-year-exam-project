b63a4c0600a9c6c1684eb9e9e5a1fbdbc5849e4c
Bad-Snowman <43063683+Bad-Snowman@users.noreply.github.com>
2019-05-16 12:21:28 +0200

    Classes and html

--- src/main/java/com/example/demo/Controller/HomeController.java
+++ src/main/java/com/example/demo/Controller/HomeController.java
@@ -1,13 +1,17 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Model.SessionTime;
 import com.example.demo.Service.EmployeeService;
 import com.example.demo.Service.ProjectManagerService;
 import com.example.demo.Service.ProjectService;
-import com.example.demo.Service.SessionService;
+import com.example.demo.Service.SessionTimeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -19,23 +23,50 @@ public class HomeController {
     @Autowired
     ProjectService projectService;
     @Autowired
-    SessionService sessionService;
+    SessionTimeService sessionTimeService;
+
+    /////////// Root /////////////
+    // All GetMapping and PostMapping related to the root
 
     @GetMapping("/")
-    public String index(){return "home/MainMenu";}
-
-    @GetMapping("/sessionList")
-    public String toSessionList(){
-        return "home/SessionList";
+    public String index() {
+        return "home/MainMenu";
     }
 
+
+    /////////////// Project ////////////
+// All GetMapping and PostMapping related to project
     @GetMapping("/projectList")
-    public String toProjectList(){
+    public String toProjectList() {
         return "home/ProjectList";
     }
 
+
+    ////////// Employee //////////////
+    // All GetMapping and PostMapping related to employee
     @GetMapping("employeeList")
-    public String toEmployeeList(){
+    public String toEmployeeList() {
         return "home/EmployeeList";
     }
+
+
+    /////////// SessionTime ////////////
+    // All GetMapping and PostMapping related to session
+    @GetMapping("/sessionList")
+    public String sessionList(Model model) {
+        List<SessionTime> sessionList = sessionTimeService.fetchAll();
+        model.addAttribute("sessionsTime", sessionList);
+        return "Home/SessionList";
+    }
+
+    @GetMapping ("/delete/{sessionTimeID}")
+    public String delete (@PathVariable("sessionTimeID") String sessionTimeID){
+    boolean deleted = sessionTimeService.deleteSessionTime(sessionTimeID);
+    if (deleted){
+        return "redirect:/";
+    }else{
+        return "redirect:/";
+    }
+    }
+
 }
---
+++ src/main/java/com/example/demo/Service/SessionTimeService.java
@@ -0,0 +1,23 @@
+package com.example.demo.Service;
+
+import com.example.demo.Model.SessionTime;
+import com.example.demo.Repository.SessionTimeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SessionTimeService {
+    @Autowired
+    SessionTimeRepo SessionTimeRepo;
+
+    public List<SessionTime> fetchAll(){return SessionTimeRepo.fetchAll();}
+
+
+    public boolean deleteSessionTime(String sessionTimeID){
+        return SessionTimeRepo.deleteSessionTime(sessionTimeID);
+    }
+}
+
+
