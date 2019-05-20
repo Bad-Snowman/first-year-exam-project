c5e9c70d0caba3af6273232768ff97857ba856a1
Bad-Snowman <43063683+Bad-Snowman@users.noreply.github.com>
2019-05-20 11:04:03 +0200

    CSS work and a getCurrentTimeByUsingDate funktion for createSession

--- src/main/java/com/example/demo/Repository/SessionTimeRepo.java
+++ src/main/java/com/example/demo/Repository/SessionTimeRepo.java
@@ -7,13 +7,16 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public class SessionTimeRepo {
     @Autowired
     JdbcTemplate template;
-
 
     //Handles sql statements
     public List<SessionTime> fetchAll(){
@@ -42,10 +45,16 @@ public class SessionTimeRepo {
         return null;
     }
 
-
     public boolean deleteSessionTime(String sessionTimeID) {
         String sql = "DELETE FROM sessionTime WHERE sessionTimeID=?";
         return template.update(sql, sessionTimeID) > 0;
+    }
+
+    public static void getCurrentTimeUsingDate() {
+        Date date = new Date();
+        String strDateFormat = "hh:mm:ss a";
+        DateFormat dateFormat = new SimpleDateFormat(strDateFormat);
+        String formattedDate= dateFormat.format(date);
     }
 
 }
