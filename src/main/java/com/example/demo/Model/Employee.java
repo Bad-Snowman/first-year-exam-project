dfcceeac94a2b360a1a1a23aadcb82ba3a6c35f3
Lasse <thedragonitee@gmail.com>
2019-05-22 14:33:47 +0200

    har ændret mellemrum

--- src/main/java/com/example/demo/Model/Employee.java
+++ src/main/java/com/example/demo/Model/Employee.java
@@ -2,6 +2,8 @@ package com.example.demo.Model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Employee {
@@ -9,6 +11,8 @@ public class Employee {
     private int employeeID;
     private String employeeFirstName;
     private String employeeLastName;
+    @NotNull
+    @Size(min=10000000, max=100000000)
     private int employeePhone;
     private String employeeEmail;
 
